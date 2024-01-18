9753515e1d6f8481e33398e6c75aab42f261e9cd
DevZenBook <julien.mangeat@mail-formateur.net>
2024-01-18 12:20:24 +0100

    ajout d'une pseudo interface à home_screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-    @override
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-            title: const Text('Gestion Commandes Traiteur'),
-        ),
-        body: const Center(
-            child: Text('Bienvenue dans l\'application de gestion des commandes traiteur !'),
-        ),
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gestion Commandes Traiteur'),
+      ),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () => {},
+            child: const Text('Catalogue des plats'),
+          ),
+          ElevatedButton(
+            onPressed: () => {},
+            child: const Text('Gestion des commandes'),
+          ),
+          ElevatedButton(
+            onPressed: () => {},
+            child: const Text('Espace client'),
+          ),
+        ],
+      )),
+    );
+  }
 }
